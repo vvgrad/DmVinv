b44e0dd376a0b8e8471b746bfc52bc5d21cf97b8
dimvind <dimvind@gmail.com>
2026-03-31 15:26:46 +0300

    sdsd

--- js.js
+++ js.js
@@ -1,15 +1,25 @@
+let lastScroll = window.scrollY;
+
 const elements = document.querySelectorAll('.hidden');
 
 const observer = new IntersectionObserver((entries) => {
+  const currentScroll = window.scrollY;
+  const isScrollingUp = currentScroll < lastScroll;
+
   entries.forEach(entry => {
     if (entry.intersectionRatio >= 0.5) {
       entry.target.classList.add('visible');
+    } else {
+      // удаляем только если скролл вверх
+      if (isScrollingUp) {
+        entry.target.classList.remove('visible');
+      }
     }
   });
+
+  lastScroll = currentScroll;
 }, {
   threshold: 0.5
 });
 
 elements.forEach(el => observer.observe(el));
-
-
